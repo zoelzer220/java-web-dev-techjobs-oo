65d2fcc25f5047879d7e6c95568489e6eaa0f933
zoelzer220 <mmp6301@gmail.com>
2020-09-21 22:02:56 -0500

    Java Assignment 2 Completed DRYING & Base Class

--- src/org/launchcode/techjobs_oo/Tests/JobTests.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTests.java
@@ -1,14 +1,13 @@
 package org.launchcode.techjobs_oo.Tests;
+
 import org.launchcode.techjobs_oo.Job;
 import org.launchcode.techjobs_oo.Employer;
 import org.launchcode.techjobs_oo.Location;
 import org.launchcode.techjobs_oo.PositionType;
 import org.launchcode.techjobs_oo.CoreCompetency;
 
-
-// import org.junit.Before;
-import org.junit.Test;
-
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,11 +16,11 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class JobTests {
 
-//    @Before
-//    public static void initializeJobs() {
-//        Job jobOne = new Job();
-//        Job jobTwo = new Job();
-//    }
+    @Before
+    public void initializeJobs() {
+       Job jobOne = new Job();
+       Job jobTwo = new Job();
+    }
 
     @Test
     public void testSettingJobId() { //tests empty constructor to ensure IDs are one apart
@@ -51,11 +50,32 @@ public class JobTests {
         Job jobTwo = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         assertFalse((jobOne.equals(jobTwo)));
         assertFalse(jobOne.getId() == jobTwo.getId()); //just double checking, unnecessary
-
     }
 
+    @Test
+    public void testJobPrintoutBlankline() { //tests the beginning and end of job object string using toString
+       Job jobOne = new Job();
+       char beginning = jobOne.toString().charAt(0);
+       char end = jobOne.toString().charAt(jobOne.toString().length()-1);
+       assertTrue(beginning == end);
+    }
 
+//    @Test
+//    public void testJobPrintoutLabels() { //checks that each printout has a label for each field followed by data
+//        Job jobOne = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+//        // assertTrue data is own line
+//        // assertTrue string has label for each field
+//        // assertTrue data is after label
+//        assertEquals("-", jobOne.toString());
+//    }
 
+    @Test
+    public void testEmptyJob() { //tests that a job with only an id spits back an error message
+        Job jobOne = new Job();
+        String emptyJobTest = jobOne.toString();
+        String errorMessage = "\n OOPS! This job does not seem to exist. \n";
+        assertTrue(emptyJobTest == errorMessage);
 
+    }
 
 }
